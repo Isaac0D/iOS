d908414c984079902dba34aaed990ec975a4e0fc
Alberto Isaac Salcedo Torres <albertito076@gmail.com>
2024-09-09 10:37:26 -0600

    Agrega maquina de estados y funcionaidad de boton con operacion

--- calculadora/calculadora/ViewController.swift
+++ calculadora/calculadora/ViewController.swift
@@ -7,12 +7,20 @@
 
 import UIKit
 
+enum estadosDeLaCalculadora{
+    case seleccionarNumeros
+    case escogerOperacion
+    case mostrarResultado
+}
+
 class ViewController: UIViewController {
+    var estadoActual: estadosDeLaCalculadora = estadosDeLaCalculadora.seleccionarNumeros
     
     @IBOutlet weak var textiCambiar: UILabel!
     @IBOutlet weak var botonOperacion: UIButton!
     
     var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
+    var operacionActual: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +32,30 @@ class ViewController: UIViewController {
     
     
     @IBAction func queHacerPulsarBoton(_ sender: UIButton) {
-        let textoAñadir =
-        botones_interfaz[(sender.restorationIdentifier ?? botonOperacion.restorationIdentifier) ?? "boton"]?.numero
-        textiCambiar.text = "\(textiCambiar.text ?? "")\(textoAñadir!)"
+        if(estadoActual == estadosDeLaCalculadora.seleccionarNumeros){
+            let textoAñadir =
+            botones_interfaz[(sender.restorationIdentifier ?? botonOperacion.restorationIdentifier) ?? "boton"]?.numero
+            textiCambiar.text = "\(textiCambiar.text ?? "")\(textoAñadir!)"
+        }
+        else if (estadoActual == estadosDeLaCalculadora.escogerOperacion){
+            if let _mensajero: UIButton? = sender{
+                operacionActual = botones_interfaz[_mensajero!.restorationIdentifier ?? "boton0"]?.operacion
+            }
+            else{
+                operacionActual = nil
+            }
+         
+        }
+        
        
 
+    }
+    
+    @IBAction func botonEscogerOperacion(_ sender: UIButton){
+        if(estadoActual == estadosDeLaCalculadora.seleccionarNumeros){
+            estadoActual = estadosDeLaCalculadora.escogerOperacion
+            dibujarNumerosOperacionInterfaz()
+        }
     }
     
     func inicializarCalculadora() -> Void{
@@ -37,5 +64,14 @@ class ViewController: UIViewController {
     
     func crearArregloBotones() -> Void{
         botones_interfaz = IUBotonCalculadora.crearArregloBotones(  )
+    }
+    
+    func dibujarNumerosOperacionInterfaz(){
+        if(estadoActual == estadosDeLaCalculadora.escogerOperacion){
+            
+        }
+        else if(estadoActual == estadosDeLaCalculadora.seleccionarNumeros){
+            
+        }
     }
 }

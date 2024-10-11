d2b09a9f62856e199b9591cf2afd7d71102c429a
Alberto Isaac Salcedo Torres <albertito076@gmail.com>
2024-10-11 09:20:58 -0600

    boceto 2 actualizado

---
+++ boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/vista_de_celdas/VistaDeZelda.swift
@@ -0,0 +1,14 @@
+//
+//  VistaDeZeldaCollectionViewCell.swift
+//  boceto_2_CellView
+//
+//  Created by Jadzia Gallegos on 07/10/24.
+//
+
+import UIKit
+
+class VistaDeZelda: UICollectionViewCell {
+    @IBOutlet weak var etiqueta: UILabel!
+    
+    @IBOutlet weak var cuerpo: UILabel!
+}

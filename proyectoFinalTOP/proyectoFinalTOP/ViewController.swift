1b836f66d577e24861ce95e753393620a903f94a
Alberto Isaac Salcedo Torres <81589120+Isaac0D@users.noreply.github.com>
2024-11-11 09:23:40 -0700

    proyectoFinalVer2

---
+++ proyectoFinalTOP/proyectoFinalTOP/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  proyectoFinalTOP
+//
+//  Created by alumno on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "backgroundHomeSection")
+        backgroundImage.contentMode = .scaleAspectFill
+        view.insertSubview(backgroundImage, at: 0)
+    }
+
+
+}
+
